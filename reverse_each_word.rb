831abc3c3264afa3247a7b54e2e921b639c76f24
Jason Keeling <s2jason@yahoo.com>
2020-04-05 23:54:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(string)
   original_array = string.split(" ")
   return_array = []
-  original_array.map do |string|
+  original_array.collect do |string|
     return_array << string.reverse 
   end
   return_array.join(" ")
